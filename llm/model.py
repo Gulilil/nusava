43ccf4ec05d5fddfe1b335aafa158000000ae354
Gulilil <juan.csantoso@gmail.com>
2025-02-24 02:28:46 +0700

    initiate model usage

---
+++ llm/model.py
@@ -0,0 +1,26 @@
+from llama_index.core import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    Document,
+    Settings
+)
+
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.groq import Groq
+from llama_index.llms.ollama import Ollama
+from llama_index.core.query_engine import RouterQueryEngine
+
+
+llm = Ollama(model="llama3.2",
+             temperature = 0,
+             request_timeout=120.0,
+             system_prompt="""
+  You are a helpful assistant.
+  All questions and answers should be in English.
+""")
+Settings.llm = llm
+
+# Embedding
+embed_model  = HuggingFaceEmbedding(model_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B")
+Settings.embed_model = embed_model
