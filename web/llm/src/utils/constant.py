d61a331094065e2673a50c7abbc98be589e14b83
Gulilil <juan.csantoso@gmail.com>
2025-06-04 11:04:19 +0700

    init scheduler

--- web/llm/src/utils/constant.py
+++ web/llm/src/utils/constant.py
@@ -1,12 +1,3 @@
-import os
-
-DATA_DIR_PATH = os.path.join(os.getcwd(), "llm", "data")
-DATA_DIR_PATHS = {
-  "md" : os.path.join(DATA_DIR_PATH, "md"),
-  "pdf" : os.path.join(DATA_DIR_PATH, "pdf"),
-  "json" : os.path.join(DATA_DIR_PATH, "json")
-}
-
 ACTIONS_LIST = [
   {
     "action": "follow",
