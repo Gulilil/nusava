bf825cc0a6c6b5794501ed89261d7959370b9664
Gulilil <juan.csantoso@gmail.com>
2025-06-14 16:59:31 +0700

    make file for internal_action.py

---
+++ src/llm/src/internal_action.py
@@ -0,0 +1,25 @@
+from agent.agent import Agent
+import asyncio
+import sys
+
+async def internal_action(nusava: Agent, user_id: int) -> None:
+    user_id = user_id
+    await nusava.set_user(user_id)
+    await nusava.decide_action()
+
+
+if __name__ == "__main__":
+  """
+  How to run:
+    python internal_action.py <user_id>
+  """
+
+  try:
+    args = sys.argv
+    assert len(args) == 2, "[INVALID] Invalid amount of parameters"
+
+    nusava = Agent()
+    asyncio.run(internal_action(nusava))
+  
+  except Exception as e:
+    print(f"[FAILED] Fail to run internal action: {e}")
